4afa1bbadfa994f0c4044f1157921f31b4800acd
Gabriele Fazio <Gabriele@MBP-di-Gabriele.homenet.telecomitalia.it>
2018-07-14 11:32:12 +0200

    cambiato gestionale.html per includere personalizzazioni e ondaiq

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -15,9 +15,6 @@ document.onreadystatechange = () => {
         let sectionPosition = section.offsetTop;
         // sectionPosition.top = sectionPosition.top + document.body.scrollTop;
 
-        console.log("pos: " + section.id + " " + sectionPosition);
-        console.log("scroll: " + currentScroll);
-
         // If the sectionPosition is less the the currentScroll position the div we are testing has moved above the window edge.
         // the -81 is so that it includes the navbar(81px).
         if(sectionPosition - 81 < currentScroll){
